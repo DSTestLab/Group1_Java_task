2958d3d531e6c4c5f82864a66973591bab4bb019
DSTestLab <testlabzone@gmail.com>
2025-11-13 16:47:11 +0000

    Task3 solved

---
+++ src/Dilfuza/Task_3_SwapNumbers.java
@@ -0,0 +1,22 @@
+package Dilfuza;
+
+public class Task_3_SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 20;
+
+        int num ;
+
+        num = num1;
+        num1 = num2;
+        num2 = num;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+    }
+
+}
+
