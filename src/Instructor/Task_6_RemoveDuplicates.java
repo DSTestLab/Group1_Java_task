ce998c67f656ce80c8d7924760cb861dbf810316
OwenEST <owen.constantine@cydeo.com>
2025-12-09 21:31:04 +0300

    Task 5 solution and Task 6 published.

---
+++ src/Instructor/Task_6_RemoveDuplicates.java
@@ -0,0 +1,13 @@
+package Instructor;
+
+
+//  Write a return method that can remove the duplicated values from String
+//  Ex:  removeDup("AAABBBCCC")  ==> ABC
+
+
+
+
+
+
+public class Task_6_RemoveDuplicates {
+}
